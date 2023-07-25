dbcec6e811842f30fac855689560622264050db6
y0uf0ol <82054892+y0uf0ol@users.noreply.github.com>
2023-07-25 21:52:33 +0200

    fixed links cause lazy

--- automater.py
+++ automater.py
@@ -10,21 +10,23 @@ tenant_id = input('Enter your tenant ID: ')
 users_url = 'https://graph.microsoft.com/v1.0/users'
 ca_url = 'https://graph.microsoft.com/beta/identity/conditionalAccess/policies'
 grp_url = 'https://graph.microsoft.com/v1.0/groups'
-test_ca_url = ['https://raw.githubusercontent.com/y0uf0ol/MS_CA_Automater/main/Policy_Testing/Base%20Protection/CA100_BaseProtection_%20AllUsers_RegisterSecurityInfo_AllePlattformen_UntrustedLocation_BlockAccess.json',
-'https://raw.githubusercontent.com/y0uf0ol/MS_CA_Automater/main/Policy_Testing/Base%20Protection/CA101_BaseProtection_Admins_AlleApps_AllePlattformen_RequireMFA.json',
-'https://raw.githubusercontent.com/y0uf0ol/MS_CA_Automater/main/Policy_Testing/Base%20Protection/CA103_BaseProtection_InternalUsers_AllApps_AllPlatforms_MFA.json',
-'https://raw.githubusercontent.com/y0uf0ol/MS_CA_Automater/main/Policy_Testing/Base%20Protection/CA107_BaseProtection_ExternalUsers_AllApps_AllPlatforms_RequireMFA.json',
-'https://raw.githubusercontent.com/y0uf0ol/MS_CA_Automater/main/Policy_Testing/Base%20Protection/CA200_IdentityProtection_AllUsers_AllApps_LegacyClients_AllPlatforms_BlockAccess.json',
-'https://raw.githubusercontent.com/y0uf0ol/MS_CA_Automater/main/Policy_Testing/Base%20Protection/CA300_DataProtection_Admins_AllApps_AllPlatform_ShortSign-inFrequency_AND_NeverPersistantBrowserSession.json',
-'https://raw.githubusercontent.com/y0uf0ol/MS_CA_Automater/main/Policy_Testing/Base%20Protection/CA301_DataProtection_InternalUsers_AllApps_AllPlatform_ShortSign-inFrequency.json',
-'https://raw.githubusercontent.com/y0uf0ol/MS_CA_Automater/main/Policy_Testing/Base%20Protection/CA302_DataProtection_ExternalUsers_AllApps_AllPlatform_ShortSign-inFrequency.json',
-'https://raw.githubusercontent.com/y0uf0ol/MS_CA_Automater/main/Policy_Testing/Base%20Protection/CA500_AttackSurfaceReduction_Admins_AllApps_AllPlatforms_UntrustedLocation_BlockAccess.json',
-'https://raw.githubusercontent.com/y0uf0ol/MS_CA_Automater/main/Policy_Testing/Base%20Protection/CA501_AttackSurfaceReduction_InternalUsers_AllApps_AllPlatforms_UntrustedLocation_BlockAccess.json',
-'https://raw.githubusercontent.com/y0uf0ol/MS_CA_Automater/main/Policy_Testing/Base%20Protection/CA502_AttackSurfaceReduction_Admins_AllApps_UnknownDevicePlatforms_BlockAccess.json',
-'https://raw.githubusercontent.com/y0uf0ol/MS_CA_Automater/main/Policy_Testing/Base%20Protection/CA503_AttackSurfaceReduction_InternalUsers_AllApps_UnkownDevicePlatform_BlockAccess.json',
-'https://raw.githubusercontent.com/y0uf0ol/MS_CA_Automater/main/Policy_Testing/Base%20Protection/CA504_AttackSurfaceReduction_ExternalUsers_AllApps_UnkownDevicePlatform_BlockAccess.json',
-'https://raw.githubusercontent.com/y0uf0ol/MS_CA_Automater/main/Policy_Testing/Base%20Protection/CA505_AttackSurfaceReduction_ExternalUsers_NotAllowedApps_AllPlatforms_BlockAccess.json',
-'https://raw.githubusercontent.com/y0uf0ol/MS_CA_Automater/main/Policy_Testing/Base%20Protection/CA506_AttackSurfaceReduction_SeviceAccounts_AllApps_AllPlatforms_UntrustedLocation_BlockAccess.json']
+
+## Hardcoded Shit cause im a lazy fuck
+test_ca_url = ['https://raw.githubusercontent.com/y0uf0ol/CA_Deployer/main/Base_Set//CA100_BaseProtection_%20AllUsers_RegisterSecurityInfo_AllePlattformen_UntrustedLocation_BlockAccess.json',
+'https://raw.githubusercontent.com/y0uf0ol/CA_Deployer/main/Base_Set//CA101_BaseProtection_Admins_AlleApps_AllePlattformen_RequireMFA.json',
+'https://raw.githubusercontent.com/y0uf0ol/CA_Deployer/main/Base_Set//CA103_BaseProtection_InternalUsers_AllApps_AllPlatforms_MFA.json',
+'https://raw.githubusercontent.com/y0uf0ol/CA_Deployer/main/Base_Set//CA107_BaseProtection_ExternalUsers_AllApps_AllPlatforms_RequireMFA.json',
+'https://raw.githubusercontent.com/y0uf0ol/CA_Deployer/main/Base_Set//CA200_IdentityProtection_AllUsers_AllApps_LegacyClients_AllPlatforms_BlockAccess.json',
+'https://raw.githubusercontent.com/y0uf0ol/CA_Deployer/main/Base_Set//CA300_DataProtection_Admins_AllApps_AllPlatform_ShortSign-inFrequency_AND_NeverPersistantBrowserSession.json',
+'https://raw.githubusercontent.com/y0uf0ol/CA_Deployer/main/Base_Set//CA301_DataProtection_InternalUsers_AllApps_AllPlatform_ShortSign-inFrequency.json',
+'https://raw.githubusercontent.com/y0uf0ol/CA_Deployer/main/Base_Set//CA302_DataProtection_ExternalUsers_AllApps_AllPlatform_ShortSign-inFrequency.json',
+'https://raw.githubusercontent.com/y0uf0ol/CA_Deployer/main/Base_Set//CA500_AttackSurfaceReduction_Admins_AllApps_AllPlatforms_UntrustedLocation_BlockAccess.json',
+'https://raw.githubusercontent.com/y0uf0ol/CA_Deployer/main/Base_Set//CA501_AttackSurfaceReduction_InternalUsers_AllApps_AllPlatforms_UntrustedLocation_BlockAccess.json',
+'https://raw.githubusercontent.com/y0uf0ol/CA_Deployer/main/Base_Set//CA502_AttackSurfaceReduction_Admins_AllApps_UnknownDevicePlatforms_BlockAccess.json',
+'https://raw.githubusercontent.com/y0uf0ol/CA_Deployer/main/Base_Set//CA503_AttackSurfaceReduction_InternalUsers_AllApps_UnkownDevicePlatform_BlockAccess.json',
+'https://raw.githubusercontent.com/y0uf0ol/CA_Deployer/main/Base_Set//CA504_AttackSurfaceReduction_ExternalUsers_AllApps_UnkownDevicePlatform_BlockAccess.json',
+'https://raw.githubusercontent.com/y0uf0ol/CA_Deployer/main/Base_Set//CA505_AttackSurfaceReduction_ExternalUsers_NotAllowedApps_AllPlatforms_BlockAccess.json',
+'https://raw.githubusercontent.com/y0uf0ol/CA_Deployer/main/Base_Set//CA506_AttackSurfaceReduction_SeviceAccounts_AllApps_AllPlatforms_UntrustedLocation_BlockAccess.json']
 base_url = 'https://github.com/y0uf0ol/MS_CA_Automater/blob/main/Policy_Testing/Base%20Protection/Links.md' # Base Protection Policies
 group_list = ['SG-CA-ServiceAccounts', 'SG-CA-InternalUsers']
 
